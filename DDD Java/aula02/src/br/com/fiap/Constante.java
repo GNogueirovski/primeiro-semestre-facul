71cda443e2214c1782309bde9d151aa37afa129f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-20 21:37:29 -0300

    Aula 02 Java - 20-03

---
+++ DDD Java/aula02/src/br/com/fiap/Constante.java
@@ -0,0 +1,11 @@
+package br.com.fiap;
+
+public class Constante {
+    public static void main(String[] args) {
+        final double PI = 3.1415;
+        final float GRAVIDADE_TERRESTRE = 9.8f;
+
+        System.out.println("O valor de PI é " + PI + "\nGravidade da Terra: " + GRAVIDADE_TERRESTRE );
+
+    }
+}
---
+++ DDD Java/aula02/src/br/com/fiap/Primitivo.java
@@ -0,0 +1,24 @@
+package br.com.fiap;
+
+public class Primitivo {
+    public static void main(String[] args) {
+        // declaração e inicialização de variáveis
+        char sexo = 'M';
+        byte idade = 25;
+        short codigo = 15650;
+        int alunos = 50, classes = 11;
+        long brasileiros = 216455377500L;
+        float media = 8.7f;
+        double dolar = 5.68;
+        boolean alternativa = false;
+        String nomeCompleto = "Mastercleison Pereira";
+
+        System.out.println("Sexo: " + sexo + " Idade: " + idade + " Código: " + codigo);
+        System.out.println("Média: " + media + " Alunos: " + alunos + " Turmas: " + classes);
+        System.out.println("Habitantes: " + brasileiros + " Cotação do Dólar U$: " + dolar);
+        System.out.println("Alternativa: " + alternativa);
+        System.out.println("Nome: " + nomeCompleto);
+
+
+    }
+}

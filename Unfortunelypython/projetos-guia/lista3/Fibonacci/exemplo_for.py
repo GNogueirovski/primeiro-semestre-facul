3c25988c422eea8cb1c2388e57c6261367f32f25
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-30 22:55:46 -0300

    for

---
+++ Unfortunelypython/projetos-guia/lista3/Fibonacci/exemplo_for.py
@@ -0,0 +1,11 @@
+frase = "A querida Giovanna vai dar a volta por cima e passar com graças em fronend"
+
+for letra in frase:
+    print(letra)
+
+for numero in range (200, 300):
+    print(numero)
+
+frase = "Amanhã é dia de folga"
+resultado = frase.find("Palmeiras")
+print(resultado)
